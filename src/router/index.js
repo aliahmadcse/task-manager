62161c2c076958817ff1cc011a09c4e946e2ea5e
aliahmadcse <aliahmadcse@gmail.com>
2020-07-25 12:58:41 +0500

    📦 NEW: Navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
+const isLoggedIn = true;
+
 const routes = [
     {
         path: '/',
@@ -17,27 +19,64 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import(/* webpackChunkName: "about" */ '@/views/tasks/TasksAll.vue')
+            import(
+                /* webpackChunkName: "about" */ '@/views/tasks/TasksAll.vue'
+            ),
+        beforeEnter: (to, from, next) => {
+            if (isLoggedIn) {
+                next();
+            } else {
+                next({ name: 'login' });
+            }
+        }
     },
     {
         path: '/tasks/new',
         name: 'tasks-create',
-        component: () => import('@/views/tasks/TasksCreate.vue')
+        component: () => import('@/views/tasks/TasksCreate.vue'),
+        beforeEnter: (to, from, next) => {
+            if (isLoggedIn) {
+                next();
+            } else {
+                next({ name: 'login' });
+            }
+        }
     },
     {
         path: '/tasks/:id',
         name: 'tasks-edit',
-        component: () => import('@/views/tasks/TasksEdit.vue')
+        component: () => import('@/views/tasks/TasksEdit.vue'),
+        beforeEnter: (to, from, next) => {
+            if (isLoggedIn) {
+                next();
+            } else {
+                next({ name: 'Home' });
+            }
+        }
     },
     {
         path: '/register',
         name: 'register',
-        component: () => import('@/views/authentication/Register.vue')
+        component: () => import('@/views/authentication/Register.vue'),
+        beforeEnter: (to, from, next) => {
+            if (!isLoggedIn) {
+                next();
+            } else {
+                next({ name: 'Home' });
+            }
+        }
     },
     {
         path: '/login',
         name: 'login',
-        component: () => import('@/views/authentication/Login.vue')
+        component: () => import('@/views/authentication/Login.vue'),
+        beforeEnter: (to, from, next) => {
+            if (!isLoggedIn) {
+                next();
+            } else {
+                next({ name: 'login' });
+            }
+        }
     },
     {
         path: '*',
